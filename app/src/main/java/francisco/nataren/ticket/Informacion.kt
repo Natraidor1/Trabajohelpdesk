a2ea999ea30842b8a5a1a77126820fdef4f29031
Nataren <20220594@ricaldone.edu.sv>
2024-06-18 16:57:54 -0600

    Creacion de diseño de layouts

--- app/src/main/java/francisco/nataren/ticket/Informacion.kt
+++ app/src/main/java/francisco/nataren/ticket/Informacion.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Registrate : AppCompatActivity() {
+class Informacion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registrate)
+        setContentView(R.layout.activity_informacion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
